ed1c89f6994c6fd1461937f9699d56e9711a7df7
bwhite03 <bwhite1195@yahoo.com>
2019-06-01 20:09:53 -0400

    finished item component

--- src/components/guitar_products/Item.js
+++ src/components/guitar_products/Item.js
@@ -7,8 +7,67 @@ const Item = props => {
   console.log(item);
 
   return (
-    <div>
-      <h1>{item.brand}</h1>
+    <div className="product-item">
+      <h1>{item.title}</h1>
+      <div className="item-container">
+        <div className="product-img">
+          <img src={item.itemImg} />
+        </div>
+        <div className="product-info">
+          <div className="product-description">
+            <h1>Product Description</h1>
+            <p>{item.description}</p>
+          </div>
+          <div className="product-in-stock">
+            <h1>In Stock & Ready to Ship</h1>
+            <p>
+              Most orders placed before noon ET ship same day (except weekends
+              and holidays)
+            </p>
+          </div>
+          <div className="product-icons">
+            <p>
+              <i className="fas fa-headphones-alt" /> Protect Your Gear
+            </p>
+            <p>
+              <i className="fas fa-shipping-fast" /> Free Shipping
+            </p>
+            <p>
+              <i className="fas fa-globe-americas" /> International Shipping
+            </p>
+            <p>
+              <i className="fas fa-undo" /> Returns Up To 30 Days
+            </p>
+          </div>
+          <div className="product-brand">
+            <img src={item.logo} />
+          </div>
+        </div>
+        <div className="product-cart">
+          <div className="product-questions">
+            <h1>Have Questions?</h1>
+            <p>
+              Call Us <i className="fas fa-phone" /> 555-555-5555
+            </p>
+            <p>
+              Call our experts for product info and phone-only specials or chat
+              now.
+            </p>
+          </div>
+          <div className="product-add-to-cart">
+            <h1>Price:</h1>
+            <h2>${item.price}</h2>
+            <button>Add to Cart</button>
+          </div>
+          <div className="product-shipping">
+            <i className="fas fa-store-alt" />
+            <div className="product-shipping-container">
+              <h1>Ship to Store:</h1>
+              <p>Available for Pick Up: 3-5 business days</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
